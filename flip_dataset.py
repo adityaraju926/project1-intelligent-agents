79fe390a33633e73a1b5995aec21bbbda8ce7d37
Mkhan13 <mariam13khan@gmail.com>
2026-03-10 18:46:44 -0400

    split train and test data

--- flip_dataset.py
+++ flip_dataset.py
@@ -10,10 +10,13 @@ df_flipped["Gender"] = df_flipped["Gender"].map({
     "Female": "Male"
 })
 
-# Combine original and flipped
-df_doubled = pd.concat([df, df_flipped], ignore_index=True)
+df_doubled = pd.concat([df, df_flipped], ignore_index=True) # Combine original and flipped
+df_doubled = df_doubled.sample(frac=1, random_state=42).reset_index(drop=True) # Shuffle dataset
 
-# Shuffle dataset
-df_doubled = df_doubled.sample(frac=1, random_state=42).reset_index(drop=True)
+test_size = int(0.2 * len(df_doubled)) # 20% of dataset for testing
 
-df_doubled.to_csv("data/flipped_dataset.csv", index=False)
+test_df = df_doubled.iloc[:test_size] # First 20% rows
+train_df = df_doubled.iloc[test_size:] # Remaining 80% rows
+
+train_df.to_csv("data/flipped_dataset.csv", index=False)
+test_df.to_csv("data/test.csv", index=False)
